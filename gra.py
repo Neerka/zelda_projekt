2b299cc6b0f5c020e173eaa314210c6ddbdb1566
jezierska <104886984+jezierska@users.noreply.github.com>
2022-05-22 19:40:51 +0200

    funkcje gracza
    Generalnie odpaliłam sobie też filmik jak gościu robi zelda type game i teraz to juz mi sie wszystko miesza
    I troche nie wiem czy tak z tą klasą będzie ok
    Poza tym wrzuciłam sobie jakie funkcje będę definiowała później no i jest już ruch

--- gra.py
+++ gra.py
@@ -1,2 +1,32 @@
-print('Hello world')
-print("gracze")
+import pygame
+
+pygame.init()
+screenWidth = 500
+screenHeight = 500
+
+class Player:
+    def __init__(self):
+        self.speed = 5
+        self.health_points = 100
+        self.attack_points = 100
+        self.height
+        self.width
+        self.x_position = 50
+        self.y_position = 50
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and self.x_position > self.speed:
+            self.x_position -= self.speed
+        if keys[pygame.K_RIGHT] and self.x_position < screenWidth-self.width-self.speed:
+            self.x_position += self.speed
+        if keys[pygame.K_UP] and self.y_position > self.speed:
+            self.y_position -= self.speed
+        if keys[pygame.K_DOWN] and self.y_position < screenHeight-self.height-self.speed:
+            self.y_position += self.speed
+    def attack(self):
+
+    def defense(self):
+
+    def throw_sword(self):
+
+    def set_bomb(self):
