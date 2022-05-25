af602aa36fa3740cc822bc9c7441803bee0ed1e3
Neerka <kuba.markil0220@gmail.com>
2022-05-25 09:44:11 +0200

    window + baseLoop

---
+++ gra.py
@@ -0,0 +1,21 @@
+import pygame
+pygame.init()
+
+win_width = 1280
+win_height = 780
+
+win = pygame.display.set_mode((win_width, win_height))
+pygame.display.set_caption('Zelda - projekt')
+
+run_game = True
+while run_game:
+    pygame.time.delay(16)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        run = False
+
+    win.fill((40, 40, 65))
+    pygame.display.update()
+
+pygame.quit()
