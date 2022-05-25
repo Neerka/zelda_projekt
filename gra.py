f4b922550600898e5cc23be419cb3c1491ca0010
jezierska <104886984+jezierska@users.noreply.github.com>
2022-05-25 14:44:33 +0200

    miecz

--- gra.py
+++ gra.py
@@ -7,12 +7,15 @@ screenHeight = 500
 class Player:
     def __init__(self):
         self.speed = 5
+        self.sword_length = 48
+        self.sword_width = 16
         self.health_points = 100
         self.attack_points = 100
-        self.height
-        self.width
+        self.height = 32
+        self.width = 32
         self.x_position = 50
         self.y_position = 50
+        
     def move(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and self.x_position > self.speed:
@@ -23,7 +26,10 @@ class Player:
             self.y_position -= self.speed
         if keys[pygame.K_DOWN] and self.y_position < screenHeight-self.height-self.speed:
             self.y_position += self.speed
-    def attack(self):
+    def attack(self, enemy):
+        if keys[pygame.K_A]:
+            self.x_sword = self.x_position - self.sword.length
+            self.y_sword = self.y_position - 0,25*self.width
 
     def defense(self):
 
