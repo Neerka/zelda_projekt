a73dcfc498426bdda7d7878fa05f12e594bdf7e9
Neerka <100694877+Neerka@users.noreply.github.com>
2022-05-25 10:04:29 +0200

    komentarz_FPS

--- gra.py
+++ gra.py
@@ -9,7 +9,7 @@ pygame.display.set_caption('Zelda - projekt')
 
 run_game = True
 while run_game:
-    pygame.time.delay(16)
+    pygame.time.delay(16) # to jest najbliżej 60 FPSów, ale będzie do zmiany i inaczej będziemy to odświeżać
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]:
