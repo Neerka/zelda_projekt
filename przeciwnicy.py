ba3d6151f2eca3d7cb8d73f3c8326e90aea8ce1d
MalgaMatuszczak <104892638+MalgaMatuszczak@users.noreply.github.com>
2022-06-11 13:18:39 +0200

    Update and rename gra.py to przeciwnicy.py

--- przeciwnicy.py
+++ przeciwnicy.py
@@ -1,10 +1,24 @@
+import os
+
+
 class Boss():
   def __init_(self):
     self.nazwa="Donogo"
         self.pkt_zycia=200
         self.pkt_ataku_ogniem=40
+        self.status_pocisku=False
+        
    def atak_bossa(self,Gracz):
         self.pkt_zycia_gracza-=Gracz
+      
+   def pociski_bossa():
+    if self.status_pocisku==True:
+      window.fill((255, 255, 255))
+      pygame.draw.circle(win,(255, 81, 51),
+                   [300, 300], 25, 0)
+      pygame.display.update()
+    else:
+      pass
   
 class Potwory():
   def __init__(self):
@@ -32,15 +46,21 @@ class Potwory():
       self.pkt_zycia_gracza-=self.pkt_ataku_mieczem  #wprowadzić namierzanie pozycji gracza
       print("Gracz otrzymał obrażenia.")
     while self.liczba_strzal>0:
+      if status_pocisku==True:
+        pygame.draw.rect(win, (255, 195, 11),[100, 50, 100, 30], 0)
+        pygame.display.update()
+      else:
+        pass
       self.pkt_zycia_gracza-=self.pkt_ataku_lukiem
       self.liczba_strzal-=1
       print("Gracz otrzymał obrażenia.")
+      time.sleep()
       
 class Odolda(Potwory): #miecz
   def __init__(self):
     self.nazwa="Odolda"
     self.pkt_zycia=100
-    self.pkt_ataku_mieczem=20
+    self.pkt_ataku_mieczem=10
     self.szybkosc=6
     self.odleglosc_od_gracza=0 #wprowadzić namierzanie pozycji gracza
     self.promien_ataku=50
@@ -49,7 +69,7 @@ class Black_Bow_Guard(Potwory): #łuk
   def __init__(self):
     self.nazwa="Czarny łuk"
     self.pkt_zycia=50
-    self.pkt_ataku_lukiem=15
+    self.pkt_ataku_lukiem=25
     self.szybkosc=2
     self.odleglosc_od_gracza=0 #wprowadzić namierzanie pozycji gracza
     self.promien_ataku=300
