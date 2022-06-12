886f73832e93d1b4f98abebc840d1d9f34db24f7
Neerka <100694877+Neerka@users.noreply.github.com>
2022-06-12 22:06:40 +0200

    baza gry

--- gra.py
+++ gra.py
@@ -2,22 +2,33 @@ import pygame
 pygame.init()
 
 win_width = 1280
-win_height = 780
+win_height = 768
 
 win = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption('Zelda - projekt')
 
-run_game = True
-while run_game:
-    pygame.time.delay(16) # to jest najbliżej 60 FPSów, ale będzie do zmiany i inaczej będziemy to odświeżać
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    keys = pygame.key.get_pressed()
+def exit():
+    global run_game
     if keys[pygame.K_ESCAPE]:
         run_game = False
+ 
+
+def settings():
+    global run_game, keys
+    pygame.time.delay(16)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run_game = False
+    keys = pygame.key.get_pressed()
+
+
+run_game = True
+while run_game:
+    settings()
+
+    keys = pygame.key.get_pressed()
+    exit()
 
     win.fill((40, 40, 65)) # to tylko kolorek tła
     pygame.display.update()
