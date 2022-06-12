b537927f407128faac1bac49fb3a3cfb395d0074
cineeek <105227622+cineeek@users.noreply.github.com>
2022-06-12 21:58:41 +0200

    Update gra.py

--- gra.py
+++ gra.py
@@ -1,42 +1,103 @@
-import pygame
-
-TILESIZE = 64
-win_width = 1280
-win_height = 780
-win = pygame.display.set_mode((win_width, win_height))
+class Map_Tile:
 
 
-o = pygame.image.load("obstacle.png")
-v = pygame.image.load("visible.png")
+    def __init__(self, texture: str):
+        self.texture = pygame.image.load(texture).convert_alpha()
 
 
-map = [
-[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-[o,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,o],
-[o,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,o],
-[o,v,v,v,v,v,v,v,o,o,o,o,v,v,v,v,v,v,v,o],
-[o,v,v,v,v,v,v,v,o,o,o,o,v,v,v,v,v,v,v,o],
-[o,v,v,v,v,v,v,v,v,o,o,v,v,v,v,v,v,v,v,o],
-[o,v,v,v,v,v,v,v,v,o,o,v,v,v,v,v,v,v,v,o],
-[o,v,v,v,v,v,v,v,o,o,o,o,v,v,v,v,v,v,v,o],
-[o,v,v,v,v,v,v,v,o,o,o,o,v,v,v,v,v,v,v,o],
-[o,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,o],
-[o,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,o],
-[o,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,v,o],
-[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o]
+def draw_map(map):
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            win.blit(map[i][j].texture, (j*64, i*64))
+
+
+def build_walls(map):
+    walls = []
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == W or map[i][j] == D:
+                walls.append(pygame.Rect(j*64, i*64, 64, 64))
+    return walls
+
+
+def build_finish(map):
+    finish = []
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == E:
+                finish.append(pygame.Rect(j*64, i*64, 64, 64))
+    return finish
+
+
+def find_Destructible(map):
+    destructible = []
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == D:
+                destructible.append((i, j))
+    return destructible
+
+
+def load_level(level):
+    global gracz, walls, finishes, destructible
+    walls = build_walls(maps[level])
+    gracz = Player()
+    finishes = build_finish(maps[level])
+    destructible = find_Destructible(maps[level])
+
+
+currentLevel = 0
+
+E = Map_Tile('grasstile.png')  # podłoga, po której można chodzić
+F = Map_Tile('grasstile.png')  # przejście do następnego poziomu
+W = Map_Tile('rock.png')  # stałe ściany
+D = Map_Tile('rockdestroyed.png')  # zniszczalne bombą
+
+map1 = [
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
+    [W, F, F, F, F, F, W, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, F, F, F, F, W, F, F, F, F, F, F, F, F, F, F, F, F, E],
+    [W, W, W, W, F, F, W, W, W, W, W, W, W, W, F, F, F, F, F, W],
+    [W, F, F, F, F, F, F, F, F, F, W, F, F, W, F, F, F, F, F, W],
+    [W, F, F, F, F, F, F, F, F, F, W, F, F, W, F, F, F, F, F, W],
+    [W, F, F, F, F, F, F, F, F, F, D, F, F, W, F, F, F, F, F, W],
+    [W, F, F, F, F, W, W, W, W, D, D, F, F, W, F, W, F, F, F, W],
+    [W, D, F, F, F, W, F, F, F, F, F, F, F, W, F, W, F, F, F, W],
+    [W, F, F, F, F, W, F, F, F, F, F, F, F, W, W, W, F, F, F, W],
+    [W, F, F, F, F, W, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
 ]
 
+map2 = [
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
+    [W, F, F, F, F, F, F, F, F, F, F, D, F, F, F, F, F, F, F, W],
+    [F, F, F, F, F, F, F, F, F, F, F, D, F, F, F, F, F, F, F, W],
+    [W, W, W, F, F, F, W, W, W, W, W, W, W, W, F, F, F, F, F, W],
+    [W, F, F, F, F, F, W, F, F, F, W, F, F, W, F, F, F, F, F, W],
+    [W, F, F, F, F, F, W, F, F, F, F, F, F, W, F, F, F, F, F, W],
+    [W, F, F, F, F, F, W, F, F, F, F, F, F, W, F, F, F, F, F, W],
+    [W, F, F, F, F, W, W, W, W, F, F, F, F, W, F, W, F, F, F, W],
+    [W, W, W, F, W, W, F, F, F, F, F, F, F, W, F, W, F, F, F, W],
+    [W, F, F, F, F, W, F, F, F, F, F, F, F, W, W, W, F, F, F, E],
+    [W, F, F, F, F, W, F, F, F, F, F, F, F, F, D, F, F, F, F, W],
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
+]
 
+map3 = [
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, W],
+    [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W],
+]
 
-class Level:
-    def __init__(self, map):
-        self.map = map
-        self.map_width = 20
-        self.map_height = 13
-        self.TILESIZE = 64
-    def draw_game(self):
-        """ta funkcja w teorii ma rysowac cala mape"""
-        for row in range(self.map_height):
-            for column in range(self.map_width):
-                win.blit(self.map[row][column], (column*self.TILESIZE, row*self.TILESIZE))
-
+maps = [map1, map2, map3]
+walls = build_walls(maps[currentLevel])
+load_level(currentLevel)
